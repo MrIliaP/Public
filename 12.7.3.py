51f7ce39a6cf143393959681ee9831ad350592e8
MrIliaP <pozhilenkov@gmail.com>
2022-02-24 21:13:54 +0000

    Create 12.7.3.py

---
+++ 12.7.3.py
@@ -0,0 +1,10 @@
+money = int(input("Deposit Amount:"))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+income = list(per_cent.values())
+deposit = int(income[0] * money/100), int(income[1] * money/100), int(income[2] * money/100), int(income[3] * money/100)
+
+print(list(deposit))
+print("Maximum possible income -", max(deposit))
+
+
+
