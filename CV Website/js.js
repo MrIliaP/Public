e55e77ba46cbd8f53f6a730c675490bbbd0b2254
MrIliaP <98427065+MrIliaP@users.noreply.github.com>
2023-04-06 17:05:00 +0100

    Update js.js

--- CV Website/js.js
+++ CV Website/js.js
@@ -7,15 +7,7 @@ emailListItem.addEventListener('click', () => {
   alert('Email: pozhilenkov@gmail.com');
 });
 
-// Get the "About" section element
-const aboutSection = document.querySelector('.section:nth-of-type(1)');
 
-// Add a click event listener to the "About" section
-aboutSection.addEventListener('click', () => {
-  // Toggle the visibility of the paragraph element within the "About" section
-  const aboutParagraph = aboutSection.querySelector('p');
-  aboutParagraph.classList.toggle('hidden');
-});
 
 function changeBackgroundColor() {
   const colorSelect = document.getElementById("colorSelect");
